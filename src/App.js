db29240fd66f2e534e79d85a446d6d285967947a
Mariam <mariamkhmahran@hotmail.com>
2019-10-08 22:15:41 +0200

    get posts

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React from "react";
 import { render } from "react-dom";
 import FacebookLogin from "react-facebook-login";
 
+const getData = accessToken => {
+  // create a new XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+
+  // get a callback when the server responds
+  xhr.addEventListener("load", () => {
+    // update the state of the component with the result here
+    console.log(xhr.responseText);
+  });
+  // open the request with the verb and the url
+  xhr.open(
+    "GET",
+    `https://graph.facebook.com/2316537715123791/feed?limit=5&access_token=${accessToken}`
+  );
+  // send the request
+  xhr.send();
+};
+
 const responseFacebook = response => {
-  console.log(response);
+  getData(response.accessToken);
 };
 
 class App extends React.Component {
